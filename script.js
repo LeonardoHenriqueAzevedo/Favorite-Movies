d92d38979cecbab458087671022e8d14ca6ac219
Leonardo Henrique de Azevedo <leonardohenriquedeazevedo@gmail.com>
2021-08-06 20:11:36 -0300

    Movies

--- script.js
+++ script.js
@@ -2,16 +2,17 @@ const modalOverlay = document.querySelector(".modal-overlay");
 const cards = document.querySelectorAll(".card");
 const closeModal = document.querySelector(".close-modal");
 
-closeModal.addEventListener("click", () => {
-  if (modalOverlay.classList.contains("active")) {
-    modalOverlay.classList.remove("active");
-  }
-})
-
 for (let card of cards) {
   card.addEventListener("click", () => {
+    const videoID = card.getAttribute("id");
     modalOverlay.classList.add("active");
     modalOverlay.querySelector("iframe").src = `https://www.youtube.com/embed/${videoID}`;
   })
 };
 
+closeModal.addEventListener("click", () => {
+  if (modalOverlay.classList.contains("active")) {
+    modalOverlay.classList.remove("active");
+    modalOverlay.querySelector("iframe").src = "";
+  }
+})
